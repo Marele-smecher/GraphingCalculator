8126dde57a8a0f8e3fb203f2874b98aff6377c5d
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-24 14:39:06 +0200

    Changed structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include <array>
-#include <chrono>
-#include <thread>
-
-#include <SFML/Graphics.hpp>
-
-#include <graph.hpp>
+#include "graph.hpp"
 
 constexpr uint32_t SCR_WIDTH = 800;
 constexpr uint32_t SCR_HEIGHT = 600;
@@ -14,7 +8,32 @@ int main()
 {
     std::cout << "Graphing calculator\n";
 
-    Graph graph(SCR_WIDTH, SCR_HEIGHT, "Graphing calculator");
-    graph.run();
+    std::string line;
+    Parser parser;
+
+    while (true)  {
+        std::cout << "Go to GUI?(y/N/quit)\n";
+        std::getline(std::cin, line);
+        if (line == "Y" || line == "y") {
+            Graph graph(SCR_WIDTH, SCR_HEIGHT, "Graphing calculator");
+            graph.run();
+            break;
+        } else if (line == "N" || line == "n") {
+            std::getline(std::cin, line);
+            parser.setExpression(line);
+            while (true) {
+                std::cout << "Number(or 'quit' to go to GUI or use another expression):\n";
+                std::getline(std::cin, line);
+                if (line == "quit") {
+                    break;
+                }
+                double x = std::stod(line,nullptr);
+                std::cout << parser.evaluate(x) << "\n";
+            }
+        } else if (line == "quit") {
+            break;
+        }
+    }
+    
     return 0;
 }
