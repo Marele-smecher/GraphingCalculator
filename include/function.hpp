ff2f4bc5c706ea7ee1aea1534fa41acf3d407811
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-24 10:48:48 +0200

    added function default constructor

--- include/function.hpp
+++ include/function.hpp
@@ -8,18 +8,18 @@ class Function
 {
     Parser parser;
     int pointCount;
-    std::vector<Point>  points;
+    std::vector<Point> points;
     std::string expressionName;
 public:
     Function() = default;
-    Function(const std::string&, int);
-    Function(const Function&);
+    Function(const std::string& s, int x);
+    Function(const Function& F);
 
     void clearPoints();
     void generatePoints(double minX, double maxX, double step);
 
-    friend std::ostream& operator<<(std::ostream&, Function);
-    Function& operator=(const Function&);
+    friend std::ostream& operator<<(std::ostream& out, const Function &F);
+    Function& operator=(const Function& F);
 
     ~Function() = default;
 };
