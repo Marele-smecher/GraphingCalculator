849e689948d5ada15d20a90c688bf875f3ba6d16
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-24 10:50:39 +0200

    added definitions

---
+++ src/function.cpp
@@ -0,0 +1,17 @@
+#include <function.hpp>
+
+Function::Function(const std::string s, int x) : expressionName(s), pointCount(x) 
+{}
+
+Function::Function(const Function &F) 
+{
+
+}
+
+void Function::clearPoints()
+{
+    pointCount = 0;
+    points.clear();
+}
+
+
