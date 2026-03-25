1c39573e232ce0d0d08ba12795130dd7d144a26b
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-25 13:57:43 +0200

    added functionality

--- include/parser.hpp
+++ include/parser.hpp
@@ -5,13 +5,17 @@
 class Parser
 {
     std::string expression;
+    double parseTerm(size_t &i, double x);
+    double parseFactor(size_t &i, double x);
 public:
     Parser() = default;
     explicit Parser(std::string s);
 
+    double parseExpression(size_t &i, double x);
     double evaluate(double x);
     void setExpression(const std::string &s);
     [[nodiscard]]  std::string getExpression() const;
+    // static bool valid(const std::string &s);
 
     Parser& operator=(const Parser &p);
 };
