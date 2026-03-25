12914496ef5f36a8c3a4970adbf4dff75f1f758a
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-25 13:58:55 +0200

    real use case

--- main.cpp
+++ main.cpp
@@ -1,39 +1,25 @@
+#include <cstddef>
 #include <iostream>
+#include <string>
 #include "graph.hpp"
 
-constexpr uint32_t SCR_WIDTH = 800;
-constexpr uint32_t SCR_HEIGHT = 600;
+constexpr size_t SCR_WIDTH = 800;
+constexpr size_t SCR_HEIGHT = 600;
 
 int main() 
 {
     std::cout << "Graphing calculator\n";
 
-    std::string line;
-    Parser parser;
+    std::string expr, line;
 
-    while (true)  {
-        std::cout << "Go to GUI?(y/N/quit)\n";
-        std::getline(std::cin, line);
-        if (line == "Y" || line == "y") {
-            Graph graph(SCR_WIDTH, SCR_HEIGHT, "Graphing calculator");
-            graph.run();
-            break;
-        } else if (line == "N" || line == "n") {
-            std::getline(std::cin, line);
-            parser.setExpression(line);
-            while (true) {
-                std::cout << "Number(or 'quit' to go to GUI or use another expression):\n";
-                std::getline(std::cin, line);
-                if (line == "quit") {
-                    break;
-                }
-                double x = std::stod(line,nullptr);
-                std::cout << parser.evaluate(x) << "\n";
-            }
-        } else{
-            break;
-        }
-    }
+    std::cout << "Enter a valid expression(only \'(\', \')\', \'+\',  \'-\',  \'*\',  \'/\',  \'x\'):\n";
+    std::getline(std::cin, expr);
+    std::cout << "Enter how big you want the graph to be:\n";
+    std::getline(std::cin, line);
+    const double a = std::stod(line);
+    Graph grapher(SCR_WIDTH, SCR_HEIGHT, a, "Graphing Calculator");
+    grapher.setFunction(expr);
+    grapher.run();
     
     return 0;
 }
