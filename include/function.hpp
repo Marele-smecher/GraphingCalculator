86c023266bdf1682679b60da757a4720c46fc31f
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-25 13:57:05 +0200

    reworked class

--- include/function.hpp
+++ include/function.hpp
@@ -7,16 +7,20 @@
 class Function
 {
     Parser parser;
-    int pointCount;
     std::vector<Point> points;
     std::string expressionName;
-public:
-    Function() = default;
-    Function(const std::string& s, int x);
-    // Function(const Function& F);
-
+    double a;
     void clearPoints();
     void generatePoints(double minX, double maxX, double step);
+public:
+    Function() = default;
+    explicit Function(const std::string& s, double a = 5);
+    Function(const Function &F);
+    // Function(const Function& F);
+
+    [[nodiscard]] std::string getExpression() const;
+    [[nodiscard]] std::vector<Point> getPoints() const;
+    void setExpression(const std::string &s);
 
     friend std::ostream& operator<<(std::ostream& out, const Function &F);
     Function& operator=(const Function& F);
