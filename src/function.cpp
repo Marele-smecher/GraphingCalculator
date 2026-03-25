cd1e0f8d5f9e228f9038b6c1b13aa64255e26afd
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-03-25 13:59:25 +0200

    operators, constructors, getters, setters

--- src/function.cpp
+++ src/function.cpp
@@ -1,32 +1,62 @@
 #include <function.hpp>
+#include <iostream>
 
-Function::Function(const std::string &s, int x) : parser(s), pointCount(x), points(), expressionName(s) 
+Function::Function(const std::string &s, double a) : parser(s), points(), expressionName(s), a(a)
 {
-    generatePoints(-10, 10, 0.1);
+    generatePoints(-a, a, 0.01);
+    std::cout << *this << "\n";
 }
 
-/*
 Function::Function(const Function &F) 
+    : parser(F.parser), 
+      points(F.points), 
+      expressionName(F.expressionName), 
+      a(F.a)
 {
-    
-}*/
+}
+
+void Function::clearPoints()
+{
+    points.clear();
+}
+
+std::vector<Point> Function::getPoints() const
+{
+    return points;
+}
+
+Function& Function::operator=(const Function &f)
+{
+    if (this != &f) {
+        this->expressionName = f.expressionName;
+        this->parser = f.parser;
+        this->points = f.points;
+    }
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream &out, const Function &F)
+{
+    out << "Function: " << F.expressionName << "\nPoints:\n";
+    for (const auto& p : F.points) {
+        out << p << "\n"; 
+    }
+    return out;
+}
 
 void Function::generatePoints(double minX, double maxX, double step)
 {
     points.clear();
 
-    for (double x = minX; x <= maxX; x += step) {
-        double y = parser.evaluate(x); 
-        points.emplace_back(Point(x, y));
+    const int numSteps = static_cast<int>((maxX - minX) / step) + 1;
+
+    for (int i = 0; i < numSteps; ++i) {
+        double x = minX + i * step;         
+        double y = parser.evaluate(x);
+        points.emplace_back(Point(x, y));  
     }
-
-    pointCount = points.size();
 }
 
-void Function::clearPoints()
-{
-    pointCount = 0;
-    points.clear();
-}
+
 
 
