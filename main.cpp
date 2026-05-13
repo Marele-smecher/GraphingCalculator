fb2d4e43b5a969f47ef47d722577906ac0835992
Alex Gheorghica <alexandrugheorghica2@gmail.com>
2026-05-13 15:03:27 +0300

    adaugat noi functii, exceptii

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include "graph.hpp"
+#include "exceptions.hpp"
 
 constexpr size_t SCR_WIDTH = 800;
 constexpr size_t SCR_HEIGHT = 600;
@@ -12,18 +13,35 @@ int main()
 
     std::cout << "Welcome to graphing calculator!!!\n";
 
-    std::string expr, line;
+    try {
+        std::string expr, line;
 
-    std::cout << "Enter a valid expression(only \'(\', \')\', \'+\',  \'-\',  \'*\',  \'/\',  \'x\'):\n";
-    std::getline(std::cin, expr);
-    std::cout << "Enter how big you want the graph to be:\n";
-    std::cout << "You'll still be able to adjust the size with NumpadPlus and NumpadMinus\n";
-    std::getline(std::cin, line);
-    const double a = std::stod(line);
-    Graph grapher(SCR_WIDTH, SCR_HEIGHT, a, "Graphing Calculator");
-    std::cout << grapher << "\n";
-    grapher.setFunction(expr);
-    grapher.run();
-    
+        std::cout << "Enter a valid expression (only '(', ')', '+', '-', '*', '/', 'x'):\n";
+        std::getline(std::cin, expr);
+
+        std::cout << "Enter how big you want the graph to be:\n";
+        std::cout << "You'll still be able to adjust the size with NumpadPlus and NumpadMinus\n";
+        std::getline(std::cin, line);
+
+        double a = 10;
+        if (!line.empty()) {
+            a = std::stod(line);            
+        }
+
+        Graph grapher(SCR_WIDTH, SCR_HEIGHT, a, "Graphing Calculator");
+        
+        grapher.addFunction(new ParsedFunction(expr));
+        grapher.addFunction(new PolynomialFunction({-5.0, 0.0, 1.0}));
+        grapher.addFunction(new TrigonometricFunction("sin", 2.0, 1.0));
+        
+        std::cout << grapher << "\n";
+        
+        grapher.run();
+    } catch (const GraphException& e) { // Prindem excepțiile din ierarhia noastră
+        std::cerr << "\n[Eroare Interna Grafe]: " << e.what() << "\n";
+    } catch (const std::exception& e) { // Prindem orice altceva (ex. std::stod fails)
+        std::cerr << "\n[Eroare Generala]: " << e.what() << "\n";
+    }
+
     return 0;
 }
